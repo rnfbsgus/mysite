d34ccaf23a1efcb5046d9c381748d922c2f41835
Dongjin Lee <donghs1541>
2021-10-12 16:34:12 +0900

    mysite

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.mysite.mvc.main.MainActionFactory;
+import com.douzone.mysite.mvc.user.UserActionFactory;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.mvc.ActionFactory;
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -19,7 +19,7 @@ public class MainController extends HttpServlet {
 		
 		String actionName = request.getParameter("a");
 
-		ActionFactory af = new MainActionFactory();
+		ActionFactory af = new UserActionFactory();
 		Action action = af.getAction(actionName);
 		action.execute(request, response);
 	}
